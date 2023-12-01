99addd3c8f1eb4097dfabc73b1dacfc7f287df75
Nabil8795 <khannabil575@gmail.com>
2023-12-01 22:11:54 +0530

    Logic Done

--- stegano_app/views.py
+++ stegano_app/views.py
@@ -7,6 +7,12 @@ def hide_text_in_image(image,text):
     data = text.encode('utf-8')
     return stepic.encode(image, data)
 
+def extract_text_from_image(image):
+    data = stepic.decode(image)
+    if isinstance(data, bytes):
+        return data.decode('utf-8')
+    return data
+
 def index(request):
     return render(request, 'index.html')
 
@@ -16,10 +22,17 @@ def encryption_view(request):
         image_file = request.FILES['image']
         image=Image.open(image_file)
         new_image = hide_text_in_image(image,text)
-
-    return render(request, 'encryption.html')
+        image_path = 'Encrypted_Images/' +  'new_' + image_file.name
+        new_image.save(image_path)
+        message = "Text has been encrypted in the image"
+    return render(request, 'encryption.html', locals())
 
 
 def decryption_view(request):
-    return render(request, 'decryption.html')
+    text = ""
+    if request.method == 'POST':
+        image_file = request.FILES['image']
+        image = Image.open(image_file)
+        text = extract_text_from_image(image)
+    return render(request, 'decryption.html', locals())
 
